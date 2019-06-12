307658e53df3a5060f420b0e89fe5310be4044cb
Richard <rjmpham@gmail.com>
2019-06-11 23:29:55 -0600

    aws changes

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-
+import Amplify from 'aws-amplify';
+import aws_exports from './aws-exports';
 function App() {
+  Amplify.configure(aws_exports);
   return (
     <div className="App">
       <header className="App-header">
