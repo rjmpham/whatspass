bd4850381f14e04abf15092a4519d6e42f1930c6
Richard <rjmpham@gmail.com>
2019-07-08 23:37:09 -0600

    Changed the buttons to be nicer

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 // eslint-disable-next-line no-unused-vars
 import React from 'react';
 // eslint-disable-next-line no-unused-vars
-import { Button } from 'reactstrap';
+import { makeStyles } from '@material-ui/core/styles';
+import Button from '@material-ui/core/Button';
 import './App.css';
 import Amplify from 'aws-amplify';
 import aws_exports from './aws-exports';
@@ -9,6 +10,7 @@ import aws_exports from './aws-exports';
 function App(){
     Amplify.configure(aws_exports);
     return (
+        
         <div className="MainPage">
             <div className="MainTitle">
                 What's in a password?
