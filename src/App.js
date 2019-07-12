a1ffc7d0abb923766f9953d115949823c8e3c527
Richard <rjmpham@gmail.com>
2019-07-12 09:22:48 -0600

    Added colours and some css

--- src/App.js
+++ src/App.js
@@ -1,7 +1,11 @@
 // eslint-disable-next-line no-unused-vars
 import React from 'react';
 // eslint-disable-next-line no-unused-vars
-import { makeStyles } from '@material-ui/core/styles';
+import { makeStyles, MuiThemeProvider} from '@material-ui/core/styles';
+import red from '@material-ui/core/colors/red';
+import green from '@material-ui/core/colors/green';
+import yellow from '@material-ui/core/colors/yellow';
+import { createMuiTheme } from '@material-ui/core/styles';
 import Button from '@material-ui/core/Button';
 import './App.css';
 import Amplify from 'aws-amplify';
@@ -9,6 +13,15 @@ import aws_exports from './aws-exports';
 
 function App(){
     Amplify.configure(aws_exports);
+    // defines the theme for the entire page and the buttons, perhaps decouple later?
+    const defTheme = createMuiTheme({
+        palette: {
+            warning: red,
+            primary: green,
+            secondary: red,
+            error: red,
+        },
+    });
     return (
         
         <div className="MainPage">
@@ -16,11 +29,15 @@ function App(){
                 What's in a password?
             </div> 
             <div className="TitleButtons">
-                <Button color="#841584" size="lg">Weak</Button>{' '}
-                <Button color="secondary" size="lg">Medium</Button>{' '}
-                <Button color="success" size="lg">Strong</Button>{' || '}
-                <Button color="warning" size="lg">Random</Button>
+                <MuiThemeProvider theme = {defTheme}>
+                    <Button color="secondary" size="lg">Weak</Button>{' '}
+                    <Button color="secondary" size="lg">Medium</Button>{' '}
+                    <Button color="primary" size="lg">Strong</Button>{' || '}
+                    <Button color="defult" size="lg">Random</Button>
+                </MuiThemeProvider> 
             </div>
+            <div classname="replyBox"></div>
+
         </div>
     );
 }
