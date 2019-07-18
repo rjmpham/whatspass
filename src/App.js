635138344107d07c428889dff3b27a8e134265cb
Richard <rjmpham@gmail.com>
2019-07-18 15:17:22 -0600

    Added nested Material-ui themes

--- src/App.js
+++ src/App.js
@@ -9,6 +9,9 @@ import { makeStyles, MuiThemeProvider} from '@material-ui/core/styles';
 import Divider from '@material-ui/core/Divider';
 import red from '@material-ui/core/colors/red';
 import green from '@material-ui/core/colors/green';
+import yellow from '@material-ui/core/colors/yellow';
+import cyan from '@material-ui/core/colors/cyan';
+
 
 import { createMuiTheme } from '@material-ui/core/styles';
 
@@ -20,15 +23,37 @@ import aws_exports from './aws-exports';
 
 function App(){
     Amplify.configure(aws_exports);
-    // defines the theme for the entire page and the buttons, perhaps decouple later?
+    // defines the themes for the entire page and the buttons, perhaps decouple later?
     const defTheme = createMuiTheme({
         palette: {
             warning: red,
-            primary: green,
+            primary: yellow,
             secondary: red,
             error: red,
         },
     });
+    const secondTheme = createMuiTheme({
+        palette: {
+            warning: red,
+            primary: green,
+            secondary: cyan,
+            error: red,
+        },
+    });
+    // defines 
+    const useStyles = makeStyles(theme => ({
+        root: {
+            background: theme.background,
+            border: 0,
+            fontSize: 16,
+            borderRadius: 3,
+            boxShadow: '0 3px 5px 2px rgba(255, 105, 135, .3)',
+            color: 'white',
+            height: 48,
+            padding: '0 30px',
+        },
+    }));
+
     function handelWeak(){
         display = "Pasword1!";
     }
@@ -40,7 +65,7 @@ function App(){
     }
     function handelRandom(){
         display = "0ih3erf(IJcfgyumsyt";
-    }
+    }       
     // for testing
     var display = "This is a password box";
     var Lorem = require('react-lorem-component');
@@ -55,10 +80,12 @@ function App(){
                 </div> 
                 <div className="TitleButtons">
                     <MuiThemeProvider theme = {defTheme}>
-                        <Button color="secondary" size="lg" onClick= { handelWeak() }>Weak</Button>{' '}
-                        <Button color="secondary" size="lg" onClick={ handelMedium() }>Medium</Button>{' '}
-                        <Button color="primary" size="lg" onClick={ handelStrong() }>Strong</Button>{'   ||   '}
-                        <Button color="defult" size="lg" onClick={ handelRandom() }>Random</Button>
+                        <Button color="secondary" onClick= { handelWeak() }>Weak</Button>{' '}
+                        <Button color="primary" onClick={ handelMedium() }>Medium</Button>{' '}
+                        <MuiThemeProvider theme = {secondTheme}> 
+                            <Button color="primary" onClick={ handelStrong() }>Strong</Button>{'   ||   '}
+                            <Button color="secondary" className="RandomBut" onClick={ handelRandom() }>Random</Button>
+                        </MuiThemeProvider>
                     </MuiThemeProvider> 
                 </div>
                 <div className="ReplyBox">{display}</div>
