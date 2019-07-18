e77f69bba1193e0cc957943f60c87bad11f21f57
Richard <rjmpham@gmail.com>
2019-07-17 23:14:39 -0600

    Adding button tests

--- src/App.js
+++ src/App.js
@@ -4,11 +4,15 @@ import React from 'react';
 //material ui for buttons
 // eslint-disable-next-line no-unused-vars
 import { makeStyles, MuiThemeProvider} from '@material-ui/core/styles';
+
+// eslint-disable-next-line no-unused-vars
 import Divider from '@material-ui/core/Divider';
 import red from '@material-ui/core/colors/red';
 import green from '@material-ui/core/colors/green';
 
 import { createMuiTheme } from '@material-ui/core/styles';
+
+// eslint-disable-next-line no-unused-vars
 import Button from '@material-ui/core/Button';
 import './App.css';
 import Amplify from 'aws-amplify';
@@ -25,8 +29,20 @@ function App(){
             error: red,
         },
     });
-
+    function handelWeak(){
+        display = "Pasword1!";
+    }
+    function handelMedium(){
+        display = "two)%Seperated55";
+    }
+    function handelStrong(){
+        display = "92Longer($Pref1xed3373";
+    }
+    function handelRandom(){
+        display = "0ih3erf(IJcfgyumsyt";
+    }
     // for testing
+    var display = "This is a password box";
     var Lorem = require('react-lorem-component');
 
     return (
@@ -39,13 +55,13 @@ function App(){
                 </div> 
                 <div className="TitleButtons">
                     <MuiThemeProvider theme = {defTheme}>
-                        <Button color="secondary" size="lg">Weak</Button>{' '}
-                        <Button color="secondary" size="lg">Medium</Button>{' '}
-                        <Button color="primary" size="lg">Strong</Button>{'   ||   '}
-                        <Button color="defult" size="lg">Random</Button>
+                        <Button color="secondary" size="lg" onClick= { handelWeak() }>Weak</Button>{' '}
+                        <Button color="secondary" size="lg" onClick={ handelMedium() }>Medium</Button>{' '}
+                        <Button color="primary" size="lg" onClick={ handelStrong() }>Strong</Button>{'   ||   '}
+                        <Button color="defult" size="lg" onClick={ handelRandom() }>Random</Button>
                     </MuiThemeProvider> 
                 </div>
-                <div className="ReplyBox">This is a password box</div>
+                <div className="ReplyBox">{display}</div>
             </div>
             <div className="ExplainSection">
                 <div className="Divider"><Divider variant="middle" /></div> 
